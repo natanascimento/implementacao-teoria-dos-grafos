fdcce655c466b1c9ab76f94eb0db94595103f70b
AlissonSantos17 <alisson.wiin@gmail.com>
2020-09-11 00:50:28 -0300

    Adicionando o ehRegular

--- Grafo/grafo_nao_direcionado.py
+++ Grafo/grafo_nao_direcionado.py
@@ -13,3 +13,24 @@ class GrafoNaoDirecionado():
       for j in i:
         print(j, end=' ')
       print('')
+  
+  def getAdjacentes(self):
+    for i in range(self.vertices):
+      print(f"Vértice {i} é adjacente a:")
+      for j in range(self.vertices):
+        if(self.grafo[i][j] != 0):
+          print(f'{j}, ', end='')
+      print('\n')
+  
+  def ehRegular(self):
+    adjacentes = []
+    primeiroElementoAux = 0
+    for i in range(self.vertices):
+      soma = 0
+      for j in range(self.vertices):
+        soma += self.grafo[i][j]
+    for a in range(len(adjacentes)):
+      primeiroElementoAux = adjacentes[0]
+      if(adjacentes[a] != primeiroElementoAux):
+        return False
+    return True
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from Grafo import grafo_nao_direc
+from Grafo import grafo_nao_direcionado
 
-get = grafo_nao_direc.GrafoNaoDirecionado(5)
+get = grafo_nao_direcionado.GrafoNaoDirecionado(5)
 
 get.adicionarAresta(0, 2, 1)
 get.adicionarAresta(0, 3, 1)
@@ -11,4 +11,36 @@ get.adicionarAresta(1, 3, 1)
 get.adicionarAresta(1, 4, 1)
 get.adicionarAresta(1, 4, 1)
 
-get.printarGrafo()
+rodar = True
+while rodar:
+  print ("------------MENU---------------")
+  print ("1 - Ilustrar Matriz de Adjacência")
+  print ("2 - getAdjacentes")
+  print ("3 - ehRegular")
+  print ("4 - ehCompleto")
+  print ("5 - ehConexo")
+  print ("0 - Sair")
+  print ("-------------------------------")
+  execute = input("Informe um código: ")
+  print ("-------------------------------")
+  if execute == '1':
+      get.printarGrafo()
+      print('\n')
+
+  elif execute == '2':
+      get.getAdjacentes()
+      print('\n')
+
+  elif execute == '3':
+      print(f"ehReguar?: {get.ehRegular()}")
+  
+  # elif execute == '4':
+  #     print(f"ehCompleto?: {get.ehCompleto()}")
+  
+  # elif execute == '5':
+  #     print(f"ehConexo?: {get.bfs(1)}")
+  
+  elif execute == '0':
+      run = False
+      print("Saindo...")
+      exit()
